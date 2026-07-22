e14b1da669aac43e7e9adb31105e17e87c70b74f
Niclas <1033478553@qq.com>
2026-07-22 19:53:31 +0800

    修改变量命名

--- src/batch.rs
+++ src/batch.rs
@@ -6,7 +6,7 @@ use crate::{println, sbi};
 
 static mut CURRENT_APP: usize = 0;
 static USER_BASE: usize = 0x80400000;
-static KERNEL_BASE: usize = 0x84400000;
+static USER_STACK_TOP: usize = 0x84400000;
 
 fn goto_user(context: TrapContext) -> ! {
     unsafe extern "C" {
@@ -27,6 +27,6 @@ pub fn run_next_app() -> ! {
     unsafe {
         CURRENT_APP += 1;
     }
-    let context = TrapContext::init(USER_BASE, KERNEL_BASE);
+    let context = TrapContext::init(USER_BASE, USER_STACK_TOP);
     goto_user(context);
 }
