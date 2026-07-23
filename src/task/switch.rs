c5bce9dbd7689cc1774e1bf7103a1e47232df7ff
Niclas <1033478553@qq.com>
2026-07-24 00:50:44 +0800

    格式更改

--- src/task/switch.rs
+++ src/task/switch.rs
@@ -2,7 +2,6 @@ use core::arch::global_asm;
 
 use super::context::TaskContext;
 
-
 global_asm!(include_str!("switch.S"));
 unsafe extern "C" {
     fn __switch(src: *mut TaskContext, des: *mut TaskContext);
