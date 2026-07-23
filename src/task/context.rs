ff97c30c37b49d9aa2cab98a46154179271bb84e
Niclas <1033478553@qq.com>
2026-07-24 01:01:06 +0800

    实现taskcontext

---
+++ src/task/context.rs
@@ -0,0 +1,28 @@
+
+#[repr(C)]
+#[derive(Clone, Copy)]
+pub struct TaskContext {
+    ra: usize,
+    sp: usize,
+    s: [usize; 12],
+}
+
+impl TaskContext {
+    pub fn init() -> Self {
+        TaskContext {
+            ra: 0,
+            sp: 0,
+            s: [0; 12],
+        }
+    }
+    pub fn goto_restore(sp: usize) -> Self {
+        unsafe extern "C" {
+            fn __restore(context_address: usize) -> !;
+        }
+        Self {
+            ra: __restore as *const () as usize,
+            sp: sp,
+            s: [0; 12],
+        }
+    }
+}
