5d03b9951e4af6b0013ec144ab0090a318259755
Niclas <1033478553@qq.com>
2026-07-23 17:33:33 +0800

    加入sys_yield（未实现）

--- src/syscall/mod.rs
+++ src/syscall/mod.rs
@@ -4,6 +4,7 @@ mod process;
 //use crate::println;
 const SYS_WRITE: usize = 64;
 const SYS_EXIT: usize = 93;
+const SYS_YIELD: usize = 124;
 
 pub fn sys_call(which: usize, args: [usize; 3]) -> usize {
     //println!("call from user of {} with args {:?}", which, args);
--- src/syscall/process.rs
+++ src/syscall/process.rs
@@ -1,5 +1,11 @@
-use crate::batch::run_next_app;
+use crate::task::{exit_current, run_next_task, suspend_current};
 pub fn sys_exit(exit_code: i32) -> ! {
     crate::println!("[kernel] sys_exit with code {exit_code}");
-    run_next_app();
+    exit_current();
+    run_next_task();
+}
+
+pub fn sys_yield() -> ! {
+    suspend_current();
+    run_next_task();
 }
