b7a66b6e95ea07ea27316edb4fbb0e5ff014f2f0
Niclas <1033478553@qq.com>
2026-07-25 01:06:26 +0800

    在trigger之前加载app

--- src/main.rs
+++ src/main.rs
@@ -37,7 +37,7 @@ unsafe extern "C" fn kernel_main() -> ! {
     log::debug!("This is debug info");
     log::trace!("This is a trace");
 
-    timer::init_timer();
     load_apps();
+    timer::init_timer();
     run_first_task()
 }
